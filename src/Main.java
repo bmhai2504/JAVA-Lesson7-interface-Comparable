f20778257012453bf5d10faf0c3546a26a6fb9a6
bmhai2504 <bmhai2504@gmail.com>
2023-05-24 10:56:54 +0700

    finish

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        ComparableCircle[] circles = new ComparableCircle[3];
+        circles[0] = new ComparableCircle(3.6);
+        circles[1] = new ComparableCircle();
+        circles[2] = new ComparableCircle(3.5, "indigo", false);
+
+        System.out.println("Pre-sorted: ");
+        for (ComparableCircle circle : circles){
+            System.out.println(circle);
+        }
+
+        System.out.println("After-sorted: ");
+        Arrays.sort(circles);
+        for (ComparableCircle circle: circles){
+            System.out.println(circle);
+        }
+    }
+}
